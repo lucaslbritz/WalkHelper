eeb05e0e8ded482aa6d7085e8b68b6a21c077650
lucasbritz <lucas.britz@hotmail.com>
2017-06-06 10:46:27 -0300

    Correção da definição da direção.

--- WalkHelper/app/src/main/java/com/lucasbritz/walkhelper/MainActivity.java
+++ WalkHelper/app/src/main/java/com/lucasbritz/walkhelper/MainActivity.java
@@ -1,7 +1,23 @@
 package com.lucasbritz.walkhelper;
 
+import android.Manifest;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,13 +26,16 @@ import android.os.Handler;
 import android.os.Vibrator;
 import android.speech.tts.TextToSpeech;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import static android.hardware.Sensor.TYPE_ACCELEROMETER;
@@ -24,17 +43,32 @@ import static android.hardware.Sensor.TYPE_MAGNETIC_FIELD;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
+    BeaconRepository beaconRepository = new BeaconRepository();
+
+    //----------------------------------------------------------------------------------------------
+    // BLE
+
+    private static final int PERMISSIONS_REQUEST_ALL_PERMISSIONS = 1;
+    private static final String TAG = "MainActivity";
+
+    private BluetoothAdapter mBluetoothAdapter;
+    private int REQUEST_ENABLE_BT = 1;
+    private Handler mHandler;
+    private static final long SCAN_PERIOD = 10000;
+    private BluetoothLeScanner mLEScanner;
+    private ScanSettings settings;
+    private List<ScanFilter> filters;
+    private BluetoothGatt mGatt;
+
+    //----------------------------------------------------------------------------------------------
+    // Direction
+
     private static final String RIGHT = "direita";
     private static final String LEFT = "esquerda";
     private static final String FRONT = "frente";
 
-    BeaconRepository beaconRepository = new BeaconRepository();
-
     private TextView tvLocation;
     private TextView tvRotation;
-    private TextToSpeech textToSpeech;
-    private String options;
-    private ArrayList<String> breadcrumb;
 
     private SensorManager mSensorManager;
 
@@ -50,8 +84,6 @@ public class MainActivity extends Activity implements SensorEventListener {
     private Sensor accelerometer;
     private Sensor magnetometer;
 
-    private boolean isFirstTime = true;
-    private boolean toSpeech = true;
     private boolean start = false;
     private boolean runningRight = false;
     private boolean runningLeft = false;
@@ -63,10 +95,44 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     Handler handler;
 
+    //----------------------------------------------------------------------------------------------
+    // Interaction
+
+    private TextToSpeech textToSpeech;
+    private String options;
+    private ArrayList<String> breadcrumb;
+
+    private boolean isFirstTime = true;
+    private boolean toSpeech = true;
+
+    //----------------------------------------------------------------------------------------------
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // -----------------------------------------------------------------------------------------
+        // BLE
+
+        if (needPermissions(this)) {
+            requestPermissions();
+        }
+
+        mHandler = new Handler();
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, "BLE Not Supported",
+                    Toast.LENGTH_SHORT).show();
+
+            finish();
+        }
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // -----------------------------------------------------------------------------------------
+        // Direction
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometer = mSensorManager.getDefaultSensor(TYPE_ACCELEROMETER);
@@ -77,6 +143,9 @@ public class MainActivity extends Activity implements SensorEventListener {
         tvLocation = (TextView) findViewById(R.id.tvLocation);
         tvRotation = (TextView) findViewById(R.id.tvRotation);
         breadcrumb = new ArrayList<>();
+
+        // -----------------------------------------------------------------------------------------
+        // Interaction
 
         // Test
         breadcrumb.add("1");
@@ -105,21 +174,28 @@ public class MainActivity extends Activity implements SensorEventListener {
         }
     }
 
-    private void speech(String text) {
-        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if (status != TextToSpeech.ERROR) {
-                    textToSpeech.setLanguage(Locale.getDefault());
-                    textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
-                }
-            }
-        });
-    }
-
     @Override
     protected void onResume() {
         super.onResume();
+
+        //------------------------------------------------------------------------------------------
+        // BLE
+
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        } else {
+            mLEScanner = mBluetoothAdapter.getBluetoothLeScanner();
+            settings = new ScanSettings.Builder()
+                    .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
+                    .build();
+            filters = new ArrayList<ScanFilter>();
+
+            scanLeDevice(true);
+        }
+
+        //------------------------------------------------------------------------------------------
+        // Direction
 
         mSensorManager.registerListener(this, accelerometer, 500000, 500000);
         mSensorManager.registerListener(this, magnetometer, 500000, 500000);
@@ -129,13 +205,60 @@ public class MainActivity extends Activity implements SensorEventListener {
     protected void onPause() {
         super.onPause();
 
+        //------------------------------------------------------------------------------------------
+        // BLE
+
+        if (mBluetoothAdapter != null && mBluetoothAdapter.isEnabled()) {
+            scanLeDevice(false);
+        }
+
+        //------------------------------------------------------------------------------------------
+        // Direction
+
+        mSensorManager.unregisterListener(this, accelerometer);
+        mSensorManager.unregisterListener(this, magnetometer);
+
+        //------------------------------------------------------------------------------------------
+        // Interaction
+
         if (textToSpeech != null) {
             textToSpeech.stop();
             textToSpeech.shutdown();
         }
+    }
 
-        mSensorManager.unregisterListener(this, accelerometer);
-        mSensorManager.unregisterListener(this, magnetometer);
+    @Override
+    protected void onDestroy() {
+        //------------------------------------------------------------------------------------------
+        // BLE
+
+        if (mGatt == null) {
+            return;
+        }
+        mGatt.close();
+        mGatt = null;
+
+        //------------------------------------------------------------------------------------------
+
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //------------------------------------------------------------------------------------------
+        // BLE
+
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_CANCELED) {
+                //Bluetooth not enabled.
+                finish();
+                return;
+            }
+        }
+
+        //------------------------------------------------------------------------------------------
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
@@ -170,6 +293,101 @@ public class MainActivity extends Activity implements SensorEventListener {
                 }
             }
         }
+    }
+
+    //----------------------------------------------------------------------------------------------
+    // BLE
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mLEScanner.stopScan(mScanCallback);
+                }
+            }, SCAN_PERIOD);
+
+            mLEScanner.startScan(filters, settings, mScanCallback);
+        } else {
+            mLEScanner.stopScan(mScanCallback);
+        }
+    }
+
+    private ScanCallback mScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            Log.i("callbackType", String.valueOf(callbackType));
+            Log.i("result", result.toString());
+            BluetoothDevice btDevice = result.getDevice();
+            connectToDevice(btDevice);
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            for (ScanResult sr : results) {
+                Log.i("ScanResult - Results", sr.toString());
+            }
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            Log.e("Scan Failed", "Error Code: " + errorCode);
+        }
+    };
+
+    public void connectToDevice(BluetoothDevice device) {
+        if (mGatt == null) {
+            mGatt = device.connectGatt(this, false, gattCallback);
+            scanLeDevice(false);// will stop after first device detection
+        }
+    }
+
+    private final BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
+        @Override
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            Log.i("onConnectionStateChange", "Status: " + status);
+            switch (newState) {
+                case BluetoothProfile.STATE_CONNECTED:
+                    Log.i("gattCallback", "STATE_CONNECTED");
+                    gatt.discoverServices();
+                    break;
+                case BluetoothProfile.STATE_DISCONNECTED:
+                    Log.e("gattCallback", "STATE_DISCONNECTED");
+                    break;
+                default:
+                    Log.e("gattCallback", "STATE_OTHER");
+            }
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            List<BluetoothGattService> services = gatt.getServices();
+            Log.i("onServicesDiscovered", services.toString());
+            gatt.readCharacteristic(services.get(1).getCharacteristics().get(0));
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt,
+                                         BluetoothGattCharacteristic characteristic,
+                                         int status) {
+
+            Log.i("onCharacteristicRead", characteristic.toString());
+            gatt.disconnect();
+        }
+    };
+
+    //----------------------------------------------------------------------------------------------
+
+    private void speech(String text) {
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.getDefault());
+                    textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+                }
+            }
+        });
     }
 
     public void defineDirectionTo(View view) {
@@ -407,5 +625,48 @@ public class MainActivity extends Activity implements SensorEventListener {
         angle = 360 - (angle + 360) % 360;
 
         return (int) angle;
+    }
+
+    static public boolean needPermissions(Activity activity) {
+        Log.d(TAG, "needPermissions: ");
+        return activity.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                || activity.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        Log.d(TAG, "requestPermissions: ");
+        String[] permissions = new String[] {
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+        };
+        requestPermissions(permissions, PERMISSIONS_REQUEST_ALL_PERMISSIONS);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[],
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ALL_PERMISSIONS:
+                boolean hasAllPermissions = true;
+                for (int i = 0; i < grantResults.length; ++i) {
+                    if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                        hasAllPermissions = false;
+                        Log.e(TAG, "Unable to get permission " + permissions[i]);
+                    }
+                }
+                if (hasAllPermissions) {
+                    finish();
+                } else {
+                    Toast.makeText(this,
+                            "Unable to get all required permissions", Toast.LENGTH_LONG).show();
+                    finish();
+                    return;
+                }
+                break;
+            default:
+                Log.e(TAG, "Unexpected request code");
+        }
     }
 }
